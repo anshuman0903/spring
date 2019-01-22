016df74cd7eb2b132616226d064ca92de51a782d
Anshumman <Anshumman@Anshuma>
2019-01-22 20:58:52 +0530

    fff

---
+++ src/spring/Test.java
@@ -0,0 +1,12 @@
+package spring;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="spring";
+System.out.println(s.length());
+System.out.println(s.trim());
+	}
+
+}
